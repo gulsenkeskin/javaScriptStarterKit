5a507d2c5cff6d231d82e0977e8dd87cda0dcd74
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2021-10-30 13:09:54 +0300

    destructuring example ☺

---
+++ src/app.js
@@ -0,0 +1,7 @@
+let populations=[1000,2000,3000,[4000,5000]]
+let [small,medium,high,[veryHigh,maximum]]=populations
+console.log(small)
+console.log(medium)
+console.log(high)
+console.log(veryHigh)
+console.log(maximum)
