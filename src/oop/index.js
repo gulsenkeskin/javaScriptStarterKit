32025fa39abf7402cc7fb8e2439cd527d059dbd0
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2021-10-30 14:50:19 +0300

    class inheritance example :heartbeat:

--- src/oop/index.js
+++ src/oop/index.js
@@ -18,5 +18,30 @@ console.log(Customer.bisey)
 
 console.log(customer.customerNumber)
 
+
+class IndividualCustomer extends Customer {
+    constructor(firstName, id, customerNumber) {
+        //super base class ı çalıştırır
+        super(id, customerNumber)
+        this.firstName = firstName;
+    }
+
+}
+class CorporateCustomer extends Customer {
+    constructor(componyName, id, customerNumber) {
+        super(id, customerNumber)
+        this.componyName = componyName
+    }
+}
+
+
+
+
+
+
+
+
+
+
 //değerleri atamak için prototype dediğimiz yapıyı kullanıyoruz
 //değerleri constructor ile geçiyoruz
