5f8e17392ba85e55497b5f27e45de50c50a6e436
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2021-10-30 14:41:03 +0300

    class example :maple_leaf:

---
+++ src/oop/index.js
@@ -0,0 +1,11 @@
+class Customer {
+    constructor(id, customerNumber) {
+
+    }
+
+}
+
+let customer = new Customer();
+
+//deperleri atamak için prototype dediğimiz yapıyı kullanıyoruz
+//değerleri constructor ile geçiyoruz
