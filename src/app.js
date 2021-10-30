6b26ae2050e0228419c937cf92f0e8bd72c3d024
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2021-10-30 13:30:30 +0300

    spread example :♥

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+//spread example
+//spread: ayrıştırmak : elimizdeki bir aray'i ayrıştırır
+let points=[1,2,3,545,898]
+console.log(...points)
+console.log(Math.max(...points))
+
+//destructuring example
 let populations=[1000,2000,3000,[4000,5000]]
 let [small,medium,high,[veryHigh,maximum]]=populations
 console.log(small)
