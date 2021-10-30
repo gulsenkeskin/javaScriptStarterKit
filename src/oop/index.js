c77a5d566a3fea6599d36300a2eac225e5dc00b0
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2021-10-30 14:42:55 +0300

    prototyping example :apple:

--- src/oop/index.js
+++ src/oop/index.js
@@ -5,7 +5,10 @@ class Customer {
 
 }
 
-let customer = new Customer();
+let customer = new Customer(1, "1145485");
+//prototyping
+customer.name = "gülsen"
+console.log(customer.name)
 
 //deperleri atamak için prototype dediğimiz yapıyı kullanıyoruz
 //değerleri constructor ile geçiyoruz
