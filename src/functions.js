99b5cbc00ab321adeb9c0f0ca325d7c17369e432
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2021-11-01 23:40:25 +0300

    spread and rest operator using

--- src/functions.js
+++ src/functions.js
@@ -29,3 +29,72 @@ function addToCart3(product) {
 }
 
 addToCart3(product1)
+
+
+//todooooooooooooooooooooooooooooo
+let product2 = { productName: "bisküvi,", unitPrice: 5, quantity: 30 }
+let product3 = { productName: "çikolata", unitPrice: 3, quantity: 15 }
+
+let products = [{ productName: "bisküvi,", unitPrice: 5, quantity: 30 }, { productName: "çikolata", unitPrice: 3, quantity: 15 }]
+
+let products2 = [product1, product2, product3]
+
+function addToCart4(products) {
+    console.log(products)
+}
+addToCart4(products)
+addToCart4(products2)
+
+
+//rest operatörü
+
+function add(number1, number2, number3) {
+    console.log(number1 + number2 + number3)
+}
+
+add(20, 30, 6)
+
+
+function add2(...numbers) {
+    let total = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+    console.log(total)
+
+}
+
+add2(20, 30, 6)
+add2(546, 45, 6456)
+add2(798, 987, 456)
+
+
+function add3(bisey, ...numbers) {
+    let total = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+    console.log(total)
+    console.log("bisey", bisey)
+
+
+}
+//20 bisey olur
+add3(20, 30, 6)
+//546 bisey olur
+add3(546, 45, 6456)
+
+//rest operatörü dışında başka değişkenler kullanılıcaksa bunlar başa yazılmalıdır yoksa hata veririr
+//----------------------------
+
+//spread operatörü
+let numbers = [30,
+    554, 556152, 97, 6554, 6565, 87, 88]
+console.log(Math.max(...numbers))
+
+
+//spread ayrıştırır rest toparlar
+
+
